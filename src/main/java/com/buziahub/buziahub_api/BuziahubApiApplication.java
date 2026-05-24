9852b98a9749e5a4210a5fe07a6338a4dd3ce834
pgromiec <pgromiec@gmail.com>
2026-05-24 13:55:43 +0100

    Initial project setup for BuziaHub API

---
+++ src/main/java/com/buziahub/buziahub_api/BuziahubApiApplication.java
@@ -0,0 +1,13 @@
+package com.buziahub.buziahub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuziahubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuziahubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buziahub/buziahub_api/BuziahubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buziahub.buziahub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuziahubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
